9438cf46ea474bc16155117365c9d48a5cf427f1
Yifei <yifeizhou.to@gmail.com>
2019-07-12 19:16:07 +0000

    Enhanced order details page with product details and email

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      # flash[:success] = "Welcome to Jungle!"
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,9 @@ class LineItem < ActiveRecord::Base
 
   belongs_to :order
   belongs_to :product
+  # def product
+  #   Product.find(self.product_id)
+  # end
 
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
