bed7dd405ce4b83734997deb5b19cefe832e6f04
Yifei <yifeizhou.to@gmail.com>
2019-07-13 07:47:11 +0000

    Users are able to delete their own reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
     before_action :require_login
 
     def create
-        @product = Product.find_by(id: params[:product_id])
+        @product = Product.find(params[:product_id])
         @review = Review.new(review_params)
         @review.product = @product
         @review.user = current_user
@@ -14,12 +14,20 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def destroy
+        @product = Product.find(params[:product_id])
+        @review = @product.reviews.find(params[:id])
+        @review.destroy
+
+        redirect_to @product
+    end
+
     private
 
     def review_params
         params.require(:review).permit(:rating, :description, :user_id, :product_id)
     end
-    
+
     def require_login
         unless logged_in?
         flash[:error] = "You must be logged in to access this section"
